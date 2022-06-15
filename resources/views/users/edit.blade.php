ddbdd650018b4c09c1a75a2ba7c277734563258a
fumika92 <mikikimi0715>
2022-06-15 15:47:23 +0900

    edit file useredit

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -33,9 +33,9 @@
       
       	  <!-- 自己紹介 - body -->
           <div class="sm:col-span-2">
-            <label for="email" class="inline-block text-gray-800 text-sm sm:text-base mb-2">自己紹介</label>
-            <textarea name="user[email]" class="w-full h-64 bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2">{{ $user->body }}</textarea>
-    		    <p class='email__error' style='color:red'>{{ $errors->first('user.email') }}</p>
+            <label for="body" class="inline-block text-gray-800 text-sm sm:text-base mb-2">自己紹介</label>
+            <textarea name="user[body]" class="w-full h-64 bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2">{{ $user->body }}</textarea>
+    		    <p class='body__error' style='color:red'>{{ $errors->first('user.body') }}</p>
           </div>
 
           <!-- メールアドレス - body -->
